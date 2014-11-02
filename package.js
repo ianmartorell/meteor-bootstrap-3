4a5e3c28f33e89869697c78c2af1ae1a19d07f1f
Ian Martorell <ianmartorell@gmail.com>
2014-11-02 12:05:02 +0100

    Updated to 3.3.0

--- package.js
+++ package.js
@@ -1,17 +1,16 @@
 Package.describe({
   name: 'ian:bootstrap-3',
-  summary: "Bootstrap 3.2. HTML, CSS and JS framework for developing responsive, mobile-first web projects.",
-  version: '1.0.3',
+  summary: "HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.",
+  version: '3.3.0_2',
   git: 'https://github.com/ianmartorell/meteor-bootstrap-3'
 });
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0', 'client');
 
-  api.add_files('css/bootstrap.min.css', 'client');
-  api.add_files('js/bootstrap.min.js', 'client');
+  api.add_files('css/bootstrap.css', 'client');
+  api.add_files('js/bootstrap.js', 'client');
 
-  // Fonts
   api.add_files('fonts/glyphicons-halflings-regular.eot', 'client');
   api.add_files('fonts/glyphicons-halflings-regular.ttf', 'client');
   api.add_files('fonts/glyphicons-halflings-regular.svg', 'client');
